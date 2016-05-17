8f7046f704f87393c21cf5fecbd475b05d5f015e
brktrnc <b.turanci@gmail.com>
2016-05-17 11:38:43 +0300

    Tombala

---
+++ tombala/script.js
@@ -0,0 +1,16 @@
+﻿
+
+
+
+
+$(document).ready(function () {
+
+
+    $(".none").each(function () {
+        var sayi = Math.floor((Math.random() * 99));
+      
+            $(this).append("<span>" + sayi + "</span>");      
+                
+    });
+
+});
